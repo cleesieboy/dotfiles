d058e4af8604f808f27b52d99ff59d2296ca9663
Joakim Olsen <joakim.olsen@pm.me>
2021-05-28 20:05:53 +0200

    Changed hardcoded links to home folder.

--- .fzf.zsh
+++ .fzf.zsh
@@ -1,14 +1,14 @@
 # Setup fzf
 # ---------
-if [[ ! "$PATH" == */home/jocke/.fzf/bin* ]]; then
-  export PATH="$PATH:/home/jocke/.fzf/bin"
+if [[ ! "$PATH" == *~/.fzf/bin* ]]; then
+  export PATH="$PATH:~/.fzf/bin"
 fi
 
 # Auto-completion
 # ---------------
-[[ $- == *i* ]] && source "/home/jocke/.fzf/shell/completion.zsh" 2> /dev/null
+[[ $- == *i* ]] && source "~/.fzf/shell/completion.zsh" 2> /dev/null
 
 # Key bindings
 # ------------
-source "/home/jocke/.fzf/shell/key-bindings.zsh"
+source "~/.fzf/shell/key-bindings.zsh"
 
